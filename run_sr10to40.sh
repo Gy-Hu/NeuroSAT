b47450693aea9e9e4f2558268a7f965242539387
Gary <swe1609011@gmail.com>
2025-03-06 22:47:38 +0800

    WIP: fixing mlx

--- run_sr10to40.sh
+++ run_sr10to40.sh
@@ -9,12 +9,13 @@
 python src/train.py \
   --task-name 'neurosat_4th_rnd' \
   --dim 128 \
-  --n_rounds 26 \
+  --n_rounds 15 \
   --epochs 200 \
   --n_pairs 100000 \
   --max_nodes_per_batch 12000 \
   --gen_log '/Users/apple/coding_env/NeuroSAT/log/data_maker_sr10t40.log' \
   --min_n 10 \
   --max_n 40 \
+  --val-file 'val_v40_vpb12000_b2604.pkl' \
  # --restore '/Users/apple/coding_env/NeuroSAT/model/neurosat_3rd_rnd_sr10to40_ep200_nr26_d128.pth.tar' \
- # --val-file 'val_v40_vpb12000_b2604.pkl'
+  
